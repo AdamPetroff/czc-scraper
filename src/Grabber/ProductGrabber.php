b3bb631240900b9749e180e33abc5f56b3f07e0b
Adam Petroff <adam.petroff@thefuntasty.com>
2019-04-05 10:42:51 +0200

    Better rating format and check if it's really the product we are looking for

--- src/Grabber/ProductGrabber.php
+++ src/Grabber/ProductGrabber.php
@@ -17,7 +17,15 @@ abstract class ProductGrabber implements IGrabber
         if ($dataJson = $el->$dataAttribute) {
             $productData = json_decode($dataJson, true);
 
-            $productData['rating'] = $el->find('span.rating', 0)->title;
+            $productPageUri = $el->find('h5 a', 0)->href;
+            $productHtml = file_get_html($this->url . $productPageUri);
+
+            if ($productHtml->find('span.producer-code', 0)->plaintext !== $productId) {
+                return null;
+            }
+
+            $rating = $productHtml->find('#product-detail span.rating__label', 0)->plaintext;
+            $productData['rating'] = (int)$rating;
 
             return $productData;
         }
